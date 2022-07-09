9cda9fa9f6d4488f9eb6591555fd84726a9336e6
igorkhapov <gafar13278@gmail.com>
2022-07-09 16:41:28 +0300

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SHuTiMEv2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.tbxTime.PreviewTextInput += new TextCompositionEventHandler(TbxInt_PreviewTextInput);
+        }
+
+        void TbxInt_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", $"/c shutdown -s -f -t {tbxTime.Text}");
+            MessageBox.Show("Автовыключение запланировано!", "Успешно", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", $"/c shutdown -a");
+            MessageBox.Show("Автовыключение отменено!", "Отмена", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            windowHelp winHelp = new windowHelp();
+            winHelp.Show();
+        }
+    }
+}
---
+++ windowHelp.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SHuTiMEv2
+{
+    /// <summary>
+    /// Логика взаимодействия для windowHelp.xaml
+    /// </summary>
+    public partial class windowHelp : Window
+    {
+        public windowHelp()
+        {
+            InitializeComponent();
+        }
+        
+        private void tbxHour_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbxHour.Text == "") { tbxHour.Text = "0"; }
+            double decimalHour = Convert.ToDouble(tbxHour.Text);
+            tbxSec.Text = Convert.ToString(decimalHour * 3600);
+            
+        }
+
+        private void tbxSec_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbxSec.Text == "") { tbxSec.Text = "0"; }
+            double decimalSec = Convert.ToDouble(tbxSec.Text);
+            tbxHour.Text = Convert.ToString(decimalSec / 3600);
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
